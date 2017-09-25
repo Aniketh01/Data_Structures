36aba7573b1747a8ce90b59f79fe97d152a886fa
Aniketh Girish <anikethgireesh@gmail.com>
2017-09-25 21:25:54 +0530

    Test class for all the Data Structures

---
+++ java/src/Test.java
@@ -0,0 +1,20 @@
+/**
+ * @author aniketh
+ * This is the test class for the entire DSA section.
+ */
+
+import java.io.IOException;
+
+public class Test {
+	public static void main(String[] args) throws IOException{
+		Stack s = new Stack();
+		s.push(200);
+		s.push(100);
+		s.push(10);
+		s.pop();
+		s.pop();
+		s.Display();
+		
+	}
+
+}
