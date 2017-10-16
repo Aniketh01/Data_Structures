f2fe44aeeefbfff605f2c940a04f14bbabc411ae
Aniketh Girish <anikethgireesh@gmail.com>
2017-10-16 22:13:22 +0530

    null pointer exception fixed

--- java/src/SinglyLinkedList.java
+++ java/src/SinglyLinkedList.java
@@ -25,7 +25,7 @@ public class SinglyLinkedList {
 	
 	@SuppressWarnings("null")
 	public void insertAtHead(int item) {
-		Node newNode = null;
+		Node newNode = new Node();
 		newNode.data = item;
 		newNode.next = head;
 		head = newNode;
@@ -33,7 +33,7 @@ public class SinglyLinkedList {
 	
 	@SuppressWarnings("null")
 	public void insertAtEnd(int item) {
-		Node newNode = null;
+		Node newNode = new Node();
 		newNode.data = item;
 		newNode.next = null;
 		if (head == null) {
@@ -52,7 +52,7 @@ public class SinglyLinkedList {
 	@SuppressWarnings("null")
 	public void insertAt(int item, int pos) {
 		int i = 0;
-		Node newNode = null;
+		Node newNode = new Node();
 		newNode.data = item;
 		newNode.next = null;
 		Node temp;
@@ -63,5 +63,22 @@ public class SinglyLinkedList {
 		}
 		newNode.next = temp.next;
 		temp.next = newNode;		
+	}
+	
+	public void Delete(int pos) {
+		if (head == null) {
+			System.out.println("Linked list is already empty");
+		}
+		else {
+			int i = 0;
+			Node temp;
+			temp = head;
+			
+			while(i < pos - 1) {
+				i++;
+				temp = temp.next;
+			}
+			temp.next = temp.next.next;
+		}
 	}
 }
--- java/src/Test.java
+++ java/src/Test.java
@@ -7,21 +7,27 @@ import java.io.IOException;
 
 public class Test {
 	public static void main(String[] args) throws IOException{
-		Stack s = new Stack();
-		s.push(200);
-		s.push(100);
-		s.push(10);
-		s.pop();
-		s.pop();
-//		s.Display();
+//		Stack s = new Stack();
+//		s.push(200);
+//		s.push(100);
+//		s.push(10);
+//		s.pop();
+//		s.pop();
+////		s.Display();
+//		
+//		Queue Q = new Queue();
+//		Q.Enqueue(100);
+//		Q.Enqueue(200);
+//		Q.dequeue();
+//		
+//		Q.dequeue();
+//		Q.Display();
 		
-		Queue Q = new Queue();
-		Q.Enqueue(100);
-		Q.Enqueue(200);
-		Q.dequeue();
-		
-		Q.dequeue();
-		Q.Display();
+		SinglyLinkedList list = new SinglyLinkedList();
+		list.insertAtHead(2);
+		list.insertAtHead(2);
+		list.insertAtHead(2);
+		list.Traversal();
 		
 	}
 
