b3f2abd8c0312312fc8479f84933010fa192bb84
Aniketh Girish <anikethgireesh@gmail.com>
2017-10-16 17:01:09 +0530

    Traversal functionality added

--- java/src/SinglyLinkedList.java
+++ java/src/SinglyLinkedList.java
@@ -4,8 +4,22 @@
  */
 public class SinglyLinkedList {
 	Node head;
+	Node temp;
 	
 	public SinglyLinkedList() {
 		head = null;
+	}
+	
+	public void Traversal() {
+		if (head == null) {
+			return;
+		}
+		else {
+			temp = head;
+			while(temp.next != null) {
+				System.out.println(temp.data);
+				temp = temp.next;				
+			}
+		}
 	}
 }
