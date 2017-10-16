75c9a860d2a305f73bb5f36b25e0d8397be899fa
Aniketh Girish <anikethgireesh@gmail.com>
2017-10-16 21:05:28 +0530

    Insert at pos

--- java/src/SinglyLinkedList.java
+++ java/src/SinglyLinkedList.java
@@ -48,4 +48,20 @@ public class SinglyLinkedList {
 			temp.next = newNode;
 		}
 	}
+	
+	@SuppressWarnings("null")
+	public void insertAt(int item, int pos) {
+		int i = 0;
+		Node newNode = null;
+		newNode.data = item;
+		newNode.next = null;
+		Node temp;
+		temp = head;
+		while (i < pos - 1) {
+			i++;
+			temp = temp.next;
+		}
+		newNode.next = temp.next;
+		temp.next = newNode;		
+	}
 }
